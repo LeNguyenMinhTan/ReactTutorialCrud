88a2eed50f4fb0d80ebca290d2ddb1809c406f7a
LeNguyenMinhTan <tan.08.07.1997@gmail.com>
2021-08-10 23:52:16 +0700

    update project smp

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import BookForm from './BookForm';
 
-const AddBook = () => {
+const AddBook = ({ history, books, setBooks }) => {
   const handleOnSubmit = (book) => {
-    console.log(book);
+    setBooks([book, ...books]);
+    history.push('/');
   };
 
   return (
-    <React.Fragment>
+    <>
       <BookForm handleOnSubmit={handleOnSubmit} />
-    </React.Fragment>
+    </>
   );
 };
 
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,18 +1,40 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Header from '../components/Header';
 import AddBook from '../components/AddBook';
-import BooksList from '../components/BookList';
+import BooksList from '../components/BooksList';
+import useLocalStorage from '../hooks/useLocalStorage';
+import EditBook from '../components/EditBook';
 
 const AppRouter = () => {
+  const [books, setBooks] = useLocalStorage('books', []);
+
   return (
     <BrowserRouter>
       <div>
         <Header />
         <div className="main-content">
           <Switch>
-            <Route component={BooksList} path="/" exact={true} />
-            <Route component={AddBook} path="/add" />
+            <Route
+              render={(props) => (
+                <BooksList {...props} books={books} setBooks={setBooks} />
+              )}
+              path="/"
+              exact={true}
+            />
+            <Route
+              render={(props) => (
+                <AddBook {...props} books={books} setBooks={setBooks} />
+              )}
+              path="/add"
+            />
+            <Route
+              render={(props) => (
+                <EditBook {...props} books={books} setBooks={setBooks} />
+              )}
+              path="/edit/:id"
+            />
+            <Route component={() => <Redirect to="/" />} />
           </Switch>
         </div>
       </div>
